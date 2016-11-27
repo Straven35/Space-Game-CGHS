c6bb9190c119dd64704eb4929ea3588c826fd391
BuildTools <unconfigured@null.spigotmc.org>
2016-11-26 21:58:25 -0800

    Development 0.0.5
    Project Files

---
+++ 2D Space Game/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour {
+
+	public float maxHealth = 100;
+	public float curHealth = 100;
+	public Text Health;
+
+	void Update () {
+		if (curHealth == 0) {
+			Destroy (gameObject);
+		}
+
+		Health.text = "Health: " + curHealth;
+
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+
+		if (col.gameObject.tag == "Hit") {
+			AdjustCurrentHealth (-10);
+		}
+	}
+
+	public void AdjustCurrentHealth(int adj){
+
+		curHealth += adj;
+
+		if (curHealth < 0)
+			curHealth = 0;
+
+		if (curHealth > maxHealth)
+			curHealth = maxHealth;
+
+		if (maxHealth < 1)
+			maxHealth = 1;
+	}
+}
---
+++ 2D Space Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+	public Transform mainMenu;
+
+	public void StartGame (string name) {
+
+		SceneManager.LoadScene (name, LoadSceneMode.Single);
+	
+	}
+	public void QuitGame () {
+
+		Application.Quit();
+	
+	}
+	//public void OptionsMenu(bool clicked) {
+		//if (clicked == true) {
+		//	optionsMenu.gameObject.SetActive (clicked);
+		//	mainMenu.gameObject.SetActive (false);
+	//	} else {
+		//	optionsMenu.gameObject.SetActive (clicked);
+	//		mainMenu.gameObject.SetActive (true);
+	//	}
+//	}
+}
